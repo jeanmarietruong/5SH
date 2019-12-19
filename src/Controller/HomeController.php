a964b1edb7a246110aab4ed497504fcfe8d0e18c
Edouard <edouard.neveu67@gmail.com>
2019-12-19 18:48:24 +0100

    commit Ed 18h48

--- src/Controller/HomeController.php
+++ src/Controller/HomeController.php
@@ -3,17 +3,52 @@
 namespace App\Controller;
 
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpClient\HttpClient;
 use Symfony\Component\Routing\Annotation\Route;
 
 class HomeController extends AbstractController
 {
+
+    const LINK = 'https://www.superheroapi.com/api/907407582990145/';
     /**
      * @Route("/", name="home")
      */
     public function index()
     {
-        return $this->render('home/index.html.twig', [
-            'controller_name' => 'HomeController',
-        ]);
+        return $this->render('home/index.html.twig');
+    }
+
+    /**
+     * @Route("/profil", name="profil")
+     */
+    public function profilAccess()
+    {
+        return $this->render('home/profil.html.twig');
+    }
+
+    /**
+     * @Route("/Quizzs", name="show_quizz")
+     */
+    public function showQuizz()
+    {
+        /*$names = [];
+        $client = HttpClient::create();
+        for($i=1;$i<=50;$i++) {
+            $response = $client->request('GET', self::LINK.$i.'/image');
+            $statusCode = $response->getStatusCode();
+
+            if ($statusCode === 200) {
+//                $content = $response->getContent();
+                $names[] = $response->toArray();
+
+//                $content = $response->toArray();
+            }
+        }
+
+        var_dump($names);
+        exit();*/
+
+
+        return $this->render('home/showQuizz.html.twig');
     }
 }
